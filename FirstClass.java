6f4330d2b24937bc3385b9ad386c80df1e41237e
Shubham Singh <singhshubhamsinghgkp@gmail.com>
2024-03-23 02:09:29 -0400

    day 1 challange

---
+++ FirstClass.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class FirstClass {
+    public static void main(String args[]) {
+        // System.out.println("hello world");
+        // System.out.println("Shubham singh");
+
+        // variable
+        // int a = 15;
+        // int b = 25;
+        // String name = "shubham";
+        // int sum = a + b;
+        // System.out.println(a);
+        // System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.next(); // nextInt(), nextFloat()
+        // String str = sc.nextLine();
+        // System.out.println(name);
+        // System.out.println(str);
+
+        // ?sum of two num
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        // ? condition statement if if,else , switch , break
+
+        // int age = sc.nextInt();
+        // if (age < 18) {
+        // System.out.println("You are not eligible for voting.");
+        // } else {
+        // System.out.println(age + " You are eligible for voting.");
+        // }
+
+        // int buttom = sc.nextInt();
+
+        // switch (buttom) {
+        // case 1:
+        // System.out.println("shubham");
+        // break;
+        // case 2:
+        // System.out.println("vinay");
+        // break;
+        // case 3:
+        // System.out.println("nitin");
+        // break;
+        // default:
+        // System.out.println("invalid data");
+        // ;
+        // }
+
+        // ? loop for loop , while loop ,do while loop
+
+        // int num = sc.nextInt();
+
+        // for (int i = 0; i < num; i++) {
+        // System.out.println(i + 1 + "shubham");
+        // }
+
+        // int i = sc.nextInt();
+        // while (i < 10) {
+        // System.out.println(i);
+        // i = i + 1;
+        // }
+
+        // ? print sum of first natural number
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+    }
+}
